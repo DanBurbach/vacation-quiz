cf891fded891c6f7371a6c1e4cffde37fbc1965e
Daniel <dburbach1982@gmail.com>
2018-09-20 14:31:16 -0700

    added HTML, working on JS, and added prompt for visitors name

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,43 @@
 $(document).ready(function() {
     var person = prompt("Please enter your name:", "Your name here");
-    
+
+    var Q1 = $("input:radio[name=Q1]:checked").val();
+
+    var Q2 = $("input:radio[name=Q2]:checked").val();
+
+    var Q3 = $("input:radio[name=Q3]:checked").val();
+
+    var Q4 = $("input:radio[name=Q4]:checked").val();
+
+    var Q5 = $("input:radio[name=Q5]:checked").val();
+
+
+    if (true) {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else if {
+    } else {
+    }
+
   });
