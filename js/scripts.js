b5f840d1da1450823b9e1d2fa009d148360506ba
Daniel <dburbach1982@gmail.com>
2018-09-19 23:23:45 -0700

    added prompt for name, working on connecting to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-  $("#letter form").submit(function() {
-    var nameInput = $("input#name").val();
-    $(".name").text(nameInput);
-
-    $("#letter").show();
-
-    event.preventDefault();
+    var person = prompt("Please enter your name:", "Your name here");
+    
   });
-});
