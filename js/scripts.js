7e10ca5e5a1671dc520505f8584295343bcd8a17
Daniel <dburbach1982@gmail.com>
2018-09-20 15:22:09 -0700

    changed HTML and added all cities, changed css img sizing

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,48 @@
 $(document).ready(function() {
+  function myFunction() {
+    var x = document.getElementById("myRadio");
+    x.checked = false;
+  }
+debugger
     var person = prompt("Please enter your name:", "Your name here");
-
+debugger
     var Q1 = $("input:radio[name=Q1]:checked").val();
 
     var Q2 = $("input:radio[name=Q2]:checked").val();
 
     var Q3 = $("input:radio[name=Q3]:checked").val();
 
-    var Q4 = $("input:radio[name=Q4]:checked").val();
+    var Q3 = $("input:radio[name=Q4]:checked").val();
 
-    var Q5 = $("input:radio[name=Q5]:checked").val();
+    var Q4 = $("input:radio[name=Q5]:checked").val();
+debugger
 
-
-    if (true) {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else if {
-    } else {
+    if (Q1 === "1A") {
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else if (){
+    } else (){
     }
 
   });
